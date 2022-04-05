b62a0255aaec6c2e19dae36ed90420df6d22ec09
Dmitrii <dsss.front.91@gmail.com>
2022-04-05 20:38:05 +0300

    fix4

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const BASE_URL = `https://restcountries.com/v2/`;
-// const REQUEST_TIMEOUT = 5000;
+const REQUEST_TIMEOUT = 5000;
 
 const createAPI = () => {
   const api = axios.create({
     baseURL: BASE_URL,
-    headers: {
-      "Content-Type": `application/json`,
-    },
-    // timeout: REQUEST_TIMEOUT,
+    // headers: {
+    //   "Content-Type": `application/json`,
+    // },
+    timeout: REQUEST_TIMEOUT,
   });
 
   return api;
